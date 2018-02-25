21e6dfdf873ccaa211f11ca320fa8ec8acd41a92
doc HD <doc.divxm@gmail.com>
2018-02-25 16:06:03 +0300

    sailfish: AICPfy
    Change-Id: I91535a51734e825cee9b45fa488cc35b0419761a

--- aicp.mk
+++ aicp.mk
@@ -2,8 +2,8 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
@@ -11,11 +11,15 @@ $(call inherit-product, device/google/marlin/aosp_sailfish.mk)
 -include device/google/marlin/sailfish/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_sailfish
+PRODUCT_NAME := aicp_sailfish
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel
 TARGET_MANUFACTURER := HTC
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Davor B (LorD ClockaN), doc HD (semdoc)"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
